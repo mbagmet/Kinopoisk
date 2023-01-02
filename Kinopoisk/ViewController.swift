c347b57bf9235f1af87c4a6182aa4c7a25852fe3
Bagmet <m.bagmet@gmail.com>
2023-01-02 17:06:00 +0200

    Initial Commit

---
+++ Kinopoisk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kinopoisk
+//
+//  Created by Mikhail Bagmet on 02.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
